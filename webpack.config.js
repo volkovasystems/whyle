aa413cc61c723fa05859ebfceccc74cfceff1610
johnlenon <johnlenonmaghanoy@gmail.com>
2017-03-31 12:39:16 +0800

    Updated build procedure.

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,32 @@
+"use strict";
+
 const webpack = require( "webpack" );
-const ResolverPlugin = webpack.ResolverPlugin;
-const DirectoryDescriptionFilePlugin = ResolverPlugin.DirectoryDescriptionFilePlugin;
 const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 
 module.exports = {
 	"entry": "./whyle.support.js",
 	"resolve": {
-		"modulesDirectories": [ "bower_components", "node_modules" ]
+		"descriptionFiles": [
+			"bower.json",
+			"package.json"
+		],
+		"modules": [
+			"bower_components",
+			"node_modules"
+		],
+		"mainFields": [
+			"support",
+			"browser",
+			"module",
+			"main"
+		]
 	},
 	"module": {
-		"preLoaders": [
+		"rules": [
 			{
 				"test": /\.support\.js$/,
-				"loader": "source-map-loader"
+				"loader": "source-map-loader",
+				"enforce": "pre"
 			}
 		]
 	},
@@ -22,9 +36,6 @@ module.exports = {
 		"filename": "whyle.deploy.js"
 	},
 	"plugins": [
-		new ResolverPlugin( new DirectoryDescriptionFilePlugin( "bower.json", [ "support" ] ) ),
-		new ResolverPlugin( new DirectoryDescriptionFilePlugin( "package.json", [ "browser" ] ) ),
-		new ResolverPlugin( new DirectoryDescriptionFilePlugin( ".bower.json", [ "main" ] ) ),
 		new UglifyJsPlugin( {
 			"compress": {
 				"keep_fargs": true,
@@ -36,5 +47,5 @@ module.exports = {
 			"mangle": false
 		} )
 	],
-	"devtool": "#inline-source-map"
+	"devtool": "#source-map"
 };
